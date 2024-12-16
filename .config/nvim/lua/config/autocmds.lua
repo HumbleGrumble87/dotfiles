684b2311928980bc931b19eb4dd3ce434322a32c
humble grumble <humble@grumblejunction.io>
2024-12-16 09:22:53 -0600

    fixed autocmds

--- .config/nvim/lua/config/autocmds.lua
+++ .config/nvim/lua/config/autocmds.lua
@@ -7,6 +7,7 @@
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
 
-vim.cmd([[ set path=.,~/,~.config/ ]])
+vim.cmd([[ set path=.,~/,~/.config/, ]])
 
+--for work
 vim.cmd([[ set path+=/mnt/c/Users/jpg/OneDrive\\\ -\\\ Northeast\\\ Communications\\\ of\\\ WI.\\\ Inc/ ]])
