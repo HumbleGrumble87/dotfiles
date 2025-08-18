90aa54603f98165c5b9af2f71a1f3ba386da6bbd
humble grumble <humble@grumblejunction.io>
2025-08-18 12:58:25 -0500

    guh bye

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -2,15 +2,19 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+-- visual line mode substitution with pre-set one-eyed fighting kirby
 vim.keymap.set("x", "<leader>sq", function()
   vim.cmd([[normal! gv]])
   vim.cmd([[normal! :]])
   vim.api.nvim_feedkeys([[:'<,'>s/\v<()>/\1/g]], "n", true)
 end, { desc = "Start :'<,'>s/ substitution with \\(\\) in visual mode" })
 
-vim.keymap.set("n", "<leader>sw", function()
+-- globally change all matching words in file
+vim.keymap.set("n", "<leader>sW", function()
   local word = vim.fn.expand("<cWORD>")       -- captures the WORD under cursor (non-whitespace run)
   local escaped = vim.fn.escape(word, [[\/]]) -- escape slashes if present
   local command = string.format([[:%%s/\v<%s>//g]], escaped)
   vim.api.nvim_feedkeys(command, "n", true)
 end, { desc = "Substitute WORD under cursor across buffer" })
+
+print("Keymaps loaded")
